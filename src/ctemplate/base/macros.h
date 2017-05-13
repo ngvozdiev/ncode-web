1d6fe632a366a6a8b75a036206ea89ba072acc5e
Nik <nik.gvozdiev@gmail.com>
2017-05-13 19:00:00 +0100

    fixed compilation

--- src/ctemplate/base/macros.h
+++ src/ctemplate/base/macros.h
@@ -42,19 +42,19 @@ typedef unsigned __int64 uint64;
 
 
 // This is all to figure out endian-ness and byte-swapping on various systems
-#if defined(HAVE_ENDIAN_H)
-#include <endian.h>           // for the __BYTE_ORDER use below
-#elif defined(HAVE_SYS_ENDIAN_H)
-#include <sys/endian.h>       // location on FreeBSD
-#elif defined(HAVE_MACHINE_ENDIAN_H)
-#include <machine/endian.h>   // location on OS X
-#endif
-#if defined(HAVE_SYS_BYTEORDER_H)
-#include <sys/byteorder.h>    // BSWAP_32 on Solaris 10
-#endif
-#ifdef HAVE_SYS_ISA_DEFS_H
-#include <sys/isa_defs.h>     // _BIG_ENDIAN/_LITTLE_ENDIAN on Solaris 10
-#endif
+//#if defined(HAVE_ENDIAN_H)
+//#include <endian.h>           // for the __BYTE_ORDER use below
+//#elif defined(HAVE_SYS_ENDIAN_H)
+//#include <sys/endian.h>       // location on FreeBSD
+//#elif defined(HAVE_MACHINE_ENDIAN_H)
+//#include <machine/endian.h>   // location on OS X
+//#endif
+//#if defined(HAVE_SYS_BYTEORDER_H)
+//#include <sys/byteorder.h>    // BSWAP_32 on Solaris 10
+//#endif
+//#ifdef HAVE_SYS_ISA_DEFS_H
+//#include <sys/isa_defs.h>     // _BIG_ENDIAN/_LITTLE_ENDIAN on Solaris 10
+//#endif
 
 // MurmurHash does a lot of 4-byte unaligned integer access.  It
 // interprets these integers in little-endian order.  This is perfect
@@ -62,16 +62,6 @@ typedef unsigned __int64 uint64;
 // we do what we can to make this as efficient as possible.
 #if defined(HAVE_BYTESWAP_H)
 # include <byteswap.h>              // GNU (especially linux)
-# define BSWAP32(x)  bswap_32(x)
-#elif defined(HAVE_LIBKERN_OSBYTEORDER_H)
-# include <libkern/OSByteOrder.h>   // OS X
-# define BSWAP32(x)  OSSwapInt32(x)
-#elif defined(bswap32)              // FreeBSD
-  // FreeBSD defines bswap32 as a macro in sys/endian.h (already #included)
-# define BSWAP32(x)  bswap32(x)
-#elif defined(BSWAP_32)             // Solaris 10
-  // Solaris defines BSWSAP_32 as a macro in sys/byteorder.h (already #included)
-# define BSWAP32(x)  BSWAP_32(x)
 #elif !defined(BSWAP32)
 # define BSWAP32(x)  ((((x) & 0x000000ff) << 24) |      \
                       (((x) & 0x0000ff00) << 8)  |      \
